0e59868e74cdc4ffc889e0b5cf6a7b691c77ce03
Andrew <willis.andrew.a@gmail.com>
2018-12-26 18:40:35 -0500

    Fixes for various screen sizes

--- src/product.js
+++ src/product.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './product.css';
-import {Grid,Row,Col,Button} from 'react-bootstrap';
+import {Grid,Row,Col,Button,FormGroup,FormControl,ControlLabel,Form} from 'react-bootstrap';
 import axios from 'axios';
 
 class Product extends Component{ 
@@ -16,14 +16,18 @@ class Product extends Component{
         <Grid>
           <Row className="show-grid">
             <Col md={6} xs={6}>
-              <img src={this.props.item.imgURI} alt="ReloadImg" className="img-thumbnail"></img>  
+              <img src={this.props.item.imgURI} alt="ReloadImg" className="img-thumbnail img-thumbnail-product"></img>  
             </Col>
             <Col md={6} xs={6}>
               <div className="ProductInfo">
                 <h2>{this.props.item.name}</h2>
                 <h3>{this.props.item.price}</h3>
                 <h4>{this.props.item.description}</h4>
-                <Button className="AddToCart">ADD TO CART</Button>
+                <div className="Form">
+                  <label for="quantity">Quantity: </label>
+                  <input size = "3" type="text" name="quantity" className="Quantity"></input>
+                  <Button className="AddToCart">ADD TO CART</Button>
+                </div>
               </div>
             </Col>
           </Row>
